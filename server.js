87a062535e42419fdb3d8e6cee5d79fc163d5d61
Arkadiusz Zarzycki <arek@gacek73.hoste.pl>
2026-05-11 00:19:59 +0200

    fix china route

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
+app.get("/china.html", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "china.html"));
 });
 
